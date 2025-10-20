5339f260dc2e81dbe86765c7d9f6f8f06b223d3a
ssm <tjtkdan1072@gmail.com>
2025-10-20 15:24:41 +0900

    Formatter 를 구현하여 객체를 문자로, 문자를 객체로 바꾸는 formatter 구현

---
+++ src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,28 @@
+package hello.typeconverter.formatter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyNumberFormatterTest {
+
+    MyNumberFormatter formatter = new MyNumberFormatter();
+
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("10,000", Locale.KOREA);
+        assertThat(result).isEqualTo(10000L);
+    }
+
+    @Test
+    void print() {
+        String result = formatter.print(10000, Locale.KOREA);
+        assertThat(result).isEqualTo("10,000");
+    }
+
+}
